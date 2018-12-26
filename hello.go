0e075e410f1775bb77713821dff00c7de9be7a6e
EvanXzj <2686465170@qq.com>
2018-12-26 19:31:13 +0800

    get package form remote

--- hello.go
+++ hello.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/golang/example/stringutil"
+)
 
 func main() {
-    fmt.Println("hello, world.")
+    fmt.Println(stringutil.Reverse("!ooooooooG ,olleH"))
 }
-
